ef7e64db211f814e70f9360d56d1adece146ed94
weiyuan <weiyuan@fiiser.com>
2018-03-14 18:36:25 +0800

    update

--- mysql.js
+++ mysql.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var mysql = require('./dbcon.js');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -8,135 +7,39 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 5789);
 
-// app.get('/',function(req,res,next){
-//   var context = {};
-//   mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-//     if(err){
-//       next(err);
-//       return;
-//     }
-//     context.results = JSON.stringify(rows);
-    
-//     res.render('home', context);
-
-    
-//   });
-// });
+var data = [
+  { "name":"John", "age":31, "city":"New York" },
+  { "name":"Mary", "age":30, "city":"New York" },
+  { "name":"Tom", "age":30, "city":"New York" },
+]
+JSON.stringify(data);
 
 app.get('/',function(req,res,next){
     res.render('home');
 });
 
+app.get('/create',function(req,res,next){
+    res.render('create');
+});
 
-app.get('/retrieve-all',function(req,res,next){
-  var context = {};
-  mysql.pool.query('SELECT * FROM todo', function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = JSON.stringify(rows);
+app.get('/all',function(req,res,next){
+  return res.send(data)
+});
 
-    console.log(context.results);
-
-    res.render('home', context);
-    //return JSON.stringify(rows);
-  });  
-    
-  
+app.get('/get/:id',function(req,res,next){
+  return res.send(data[req.params.id])
 });
 
 app.get('/insert',function(req,res,next){
-  var context = {};
-  mysql.pool.query("INSERT INTO todo (`name`) VALUES (?)", [req.query.c], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Inserted id " + result.insertId;
-    
-
-
-    res.render('home',context);
-  });
+  return true
 });
 
-app.get('/delete',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DELETE FROM todo WHERE id=?", [req.query.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Deleted " + result.changedRows + " rows.";
-    res.render('home',context);
-  });
+app.get('/update/:id',function(req,res,next){
+  return true
 });
 
-
-///simple-update?id=2&name=The+Task&done=false&due=2015-12-5
-app.get('/simple-update',function(req,res,next){
-  var context = {};
-  mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
-    [req.query.name, req.query.done, req.query.due, req.query.id],
-    function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    context.results = "Updated " + result.changedRows + " rows.";
-    res.render('home',context);
-  });
-});
-
-///safe-update?id=1&name=The+Task&done=false
-app.get('/safe-update',function(req,res,next){
-  var context = {};
-  mysql.pool.query("SELECT * FROM todo WHERE id=?", [req.query.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    if(result.length == 1){
-      var curVals = result[0];
-      mysql.pool.query("UPDATE todo SET name=?, done=?, due=? WHERE id=? ",
-        [req.query.name || curVals.name, req.query.done || curVals.done, req.query.due || curVals.due, req.query.id],
-        function(err, result){
-        if(err){
-          next(err);
-          return;
-        }
-        context.results = "Updated " + result.changedRows + " rows.";
-        res.render('home',context);
-      });
-    }
-  });
-});
-
-app.get('/reset-table',function(req,res,next){
-  var context = {};
-  mysql.pool.query("DROP TABLE IF EXISTS todo", function(err){
-    var createString = "CREATE TABLE todo(" +
-    "id INT PRIMARY KEY AUTO_INCREMENT," +
-    "name VARCHAR(255) NOT NULL," +
-    "done BOOLEAN," +
-    "due DATE)";
-    mysql.pool.query(createString, function(err){
-      context.results = "Table reset";
-      res.render('home',context);
-    })
-  });
-});
-
-app.use(function(req,res){
-  res.status(404);
-  res.render('404');
-});
-
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500);
-  res.render('500');
+app.get('/delete/:id',function(req,res,next){
+  return true
 });
 
 app.listen(app.get('port'), function(){
